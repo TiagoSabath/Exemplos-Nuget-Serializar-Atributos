b14dc24103fc150a5901075c6e3c3d3e470b471d
Tiago Sabath <tiago.sabath@gmail.com>
2023-11-02 16:03:38 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using ExemplosNuget.Models;
+using Newtonsoft.Json;
+
+DateTime dataAtual = DateTime.Now;
+
+List<Venda> listaVenda = new List<Venda>();
+
+Venda V1 = new Venda(1, "Material de escritório", 25.00M, dataAtual);
+Venda V2 = new Venda(2, "Licença de Software", 125.00M, dataAtual);
+
+listaVenda.Add(V1);
+listaVenda.Add(V2);
+
+string serializado = JsonConvert.SerializeObject(listaVenda, Formatting.Indented);
+
+File.WriteAllText("Arquivos/venda.json", serializado);
+
+Console.WriteLine(serializado);
+
+
+//----------Deserializando --------
+
+string conteudoAquivo = File.ReadAllText("Arquivos/venda.json");
+
+List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoAquivo);
+
+foreach (Venda venda in listaVenda)
+{
+    Console.WriteLine($"Id: {venda.Id}, Produto: {venda.Produto}, Preço: {venda.Preco}, Data: {venda.DataVenda.ToString("dd/MM/yyyy HH:mm")}");
+}
